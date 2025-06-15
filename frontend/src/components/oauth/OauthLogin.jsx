e45092894325a1131b1673d08f416013ac0933c9
Lucas Paulo <lp152659@gmail.com>
2025-06-15 00:50:32 -0300

    Resolvendo erro de 404 ao acessar outras rotas

--- frontend/src/components/oauth/OauthLogin.jsx
+++ frontend/src/components/oauth/OauthLogin.jsx
@@ -1,17 +1,25 @@
-import {BsGoogle, BsGithub} from "react-icons/bs"
+import { BsGoogle, BsGithub } from "react-icons/bs"
 import "./OauthLogin.css"
 
 export const OauthLogin = () => {
 
+    //URL de produção
+        const BASE_URL =
+            import.meta.env.MODE === "development"
+                ? "http://localhost:8080"
+                : import.meta.env.VITE_API_URL_PRODUCTION;
+
     const handleLoginGoogle = () => {
-        window.location.href = 'http://localhost:8080/api/users/google'
+
+        window.location.href = `${BASE_URL}/api/users/google`
     }
 
     const handleLoginGitHub = () => {
-        window.location.href = 'http://localhost:8080/api/users/github'
+
+        window.location.href = `${BASE_URL}/api/users/github`
     }
 
-    return(
+    return (
 
         //Botões de login (Google e GitHub)
         <div className="btnGG">
